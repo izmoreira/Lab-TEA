7f62e1805396416975c0a5f39b110891080ab3f6
Izza Francceska Moreira Herrera (24151) <izza.moreira@est.zamorano.edu>
2022-09-08 14:14:12 -0600

    Commit versión 1

---
+++ Lab01/lab01.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovación en Tecnología Agrícola
+# Autor: izza.moreira@est.zamorano.edu
+# Fecha: 07/09/2022
+# Editado: 07/09/2022
+
+print("Hola, mundo!!!")
---
+++ Lab02/lab02-cadena.py
@@ -0,0 +1,8 @@
+# Tendencias e Innovación en Tecnologia Agricola (TEA)
+x="hello, world"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
