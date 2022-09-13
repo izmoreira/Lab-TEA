2eb06dce63f0d46f49edc3a804aa6c9d770f4c56
Izza Francceska Moreira Herrera (24151) <izza.moreira@est.zamorano.edu>
2022-09-12 20:19:07 -0600

    12-09

---
+++ Lab02/celcius2farenheit.py
@@ -0,0 +1,5 @@
+# Tendencias e Innovación en Tecnología Agricola (TEA)
+
+celsius = input("Ingrese temperatura en grados Celsius")
+farenheit = (float(celsius)* 9/5) + 32
+print("La temperatura en grados farenheit es: ", farenheit)
